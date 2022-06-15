d39fddd5c693d4d68ad48be3a3b1dc8ecd9f5225
salsabila juandira <92494551+sjuandira@users.noreply.github.com>
2022-06-15 12:47:01 +0700

    Update songtify.cpp

--- songtify.cpp
+++ songtify.cpp
@@ -1,8 +1,6 @@
 #include <iostream>
-
-// PR
-// - connect fungsi play (nunggu fungsi playlist kartika dulu)
-
+#include "addPlaylist.h"
+#include "play.h"
 
 using namespace std;
 
@@ -26,13 +24,14 @@ void SongList(string Judul, string Penyanyi, string Album, string rilis){
 	footer();
 }
 
-int Pilih(int c, int n, bool u){
+int Pilih(int c, int n, bool u, bool n1, bool n2){
 
     string pilih;
     int next = 1;
     int back = 2;
-    int home = 3;
-    int ulang = 4;
+    int add = 3;
+    int home = 4;
+    int ulang = 5;
 
     if (u == true){
 
@@ -40,11 +39,30 @@ int Pilih(int c, int n, bool u){
 
     }
 
+    if (n2 == true){
+
+        n1 = false;
+
+    }
+
+    if (n1 == true){
+
+        cout << "(yeay, Playlist berhasil dimasukkan!)" << "\n";
+
+    }
+
+    if (n2 == true){
+
+        cout << "(Music yang kamu pilih udah ada di Playlist!)" << "\n";
+
+    }
+
     if (c == 0){
 
         cout << "Pilihan :" << "\n";
         cout << "1. Next" << "\n";
-        cout << "2. Home" << "\n";
+        cout << "2. Add To Playlist" << "\n";
+        cout << "3. Home" << "\n";
         cout << "Masukan Pilihan : ";
         cin >> pilih;
 
@@ -56,7 +74,12 @@ int Pilih(int c, int n, bool u){
 
         else if (pilih == "2"){
 
-            //isi play.h
+            return add;
+
+        }
+
+        else if (pilih == "3"){
+
             return home;
 
         }
@@ -72,7 +95,8 @@ int Pilih(int c, int n, bool u){
     else if (c == n-1){
 
         cout << "1. Back" << "\n";
-        cout << "2. Home" << "\n";
+        cout << "2. Add To Playlist" << "\n";
+        cout << "3. Home" << "\n";
         cout << "Masukan Pilihan : ";
         cin >> pilih;
 
@@ -83,6 +107,12 @@ int Pilih(int c, int n, bool u){
         }
 
         else if (pilih == "2"){
+
+            return add;
+
+        }
+
+        else if (pilih == "3"){
 
             return home;
 
@@ -100,7 +130,8 @@ int Pilih(int c, int n, bool u){
 
         cout << "1. Next" << "\n";
         cout << "2. Back" << "\n";
-        cout << "3. Home" << "\n";
+        cout << "3. Add To Playlist" << "\n";
+        cout << "4. Home" << "\n";
         cout << "Masukan Pilihan : ";
         cin >> pilih;
 
@@ -117,6 +148,12 @@ int Pilih(int c, int n, bool u){
         }
 
         else if (pilih == "3"){
+
+            return add;
+
+        }
+
+        else if (pilih == "4"){
 
             return home;
 
@@ -137,15 +174,14 @@ int main(){
 	int n=10;
 	
 	string Judul[n]={"YOU WERE BEAUTIFUL","UNTIL I FOUND YOU","FLY ME TO THE MOON (REMASTERED)","ILYSB-STRIPPED","REMAJA",
-			"925","HERE WE GO AGAIN","ENCHANTED","PAPER RINGS","GORGEOUS"};
+					"925","HERE WE GO AGAIN","ENCHANTED","PAPER RINGS","GORGEOUS"};
 	string Penyanyi[n]={"DAY6","Stephen Sanchez","Frank Sinatra","LANY","Hivi!","Ardhito Pramono","Ardhito Pramono","Taylor Swift",
-			"Taylor Swift","Taylor Swift"};
+						"Taylor Swift","Taylor Swift"};
 	string Album[n]={"Sunrise","Until I Found You","FLY ME TO THE MOON (REMASTERED)","LANY","Kereta Kencan","Craziest thing happened in my backyard",
-			"Craziest thing happened in my backyard","Speak Now","Lover","Reputation"};
+					"Craziest thing happened in my backyard","Speak Now","Lover","Reputation"};
 	string rilis[n]={"2016","2021","2008","2017","2017","2020","2020","2010","2019","2017"};
 	
     bool exit=false;
-    //inisisasi stacknya disini
 
 	do{
 
@@ -154,24 +190,26 @@ int main(){
         system("cls");  
         header();
         //pilihan
-        cout<<"	\t    1. Lihat Lagu \n \t\t    2. Putar Playlist\n \t\t  3. Exit"<<"\n";
+        cout<<"	\t    1. Lihat Lagu \n \t\t    2. Putar Playlist\n \t\t    3. Exit"<<"\n";
         cout<<"-----------------------------------------------------"<<"\n";
         cout<<"	\t    Masukan Pilihan: "; cin>>pil;
 
         if(pil==1){
 
             bool returnToHome = false;
-            bool ulang= false;
+            bool ulang= false, notif1 = false;
             int pilih, counter=0;
 
             do{
 
                 SongList(Judul[counter],Penyanyi[counter],Album[counter],rilis[counter]);
-                pilih = Pilih(counter, n, ulang);
+                pilih = Pilih(counter, n, ulang, notif1, notifSongHasAddedToPlaylist);
                 if (pilih == 1){
 
                     counter += 1;
                     ulang = false;
+                    notif1 = false;
+                    notifSongHasAddedToPlaylist = false;
 
                 }
 
@@ -179,10 +217,21 @@ int main(){
 
                     counter -= 1;
                     ulang = false;
+                    notif1 = false;
+                    notifSongHasAddedToPlaylist = false;
 
                 }
 
                 else if (pilih == 3){
+
+                    // cout << "stack...";
+                    //stack.push Judul[counter],Penyanyi[counter]
+                    addSong(Judul[counter], Penyanyi[counter]);
+                    notif1 = true;
+
+                }
+
+                else if (pilih == 4){
 
                     returnToHome = true;
 
@@ -191,18 +240,28 @@ int main(){
                 else{
 
                     ulang = true;
+                    notif1 = false;
+                    notifSongHasAddedToPlaylist = false;
 
                 }
 
             }while(returnToHome == false);
         }
 
-        // // play playlist song
-        // else if pil == 2{
+        // play playlist song
+        else if (pil == 2){
+            
+            vector<string> song = playlistSong();
+            vector<string> artist = playlistSong();
 
+            if (song.empty() == false){
 
+                Play(song, artist, playlistSize);
 
-        // }
+            }
+            
+           
+        }
 
         else if (pil == 3){
 
@@ -214,4 +273,3 @@ int main(){
 
 	return 0;
 }
-
