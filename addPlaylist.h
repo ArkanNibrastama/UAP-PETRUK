ce32d259e4d595e53d5b1e164cfa903db496de02
Kartika Sari <92368462+Kartikasaei@users.noreply.github.com>
2022-06-21 12:17:34 +0700

    Update addPlaylist.h

--- addPlaylist.h
+++ addPlaylist.h
@@ -1,63 +1,45 @@
 #include <iostream>
 #include <vector>
-
 using namespace std;
 
-vector<string> judulLagu;
-vector<string> penyanyi;
+vector <string> judulLagu;
+vector <string> Penyanyi;
 int playlistSize = 0;
 
 bool notifSongHasAddedToPlaylist = false;
 
-void addSong(string song, string artist){
-
-
-    for (auto i = judulLagu.begin(); i != judulLagu.end(); i++){
+void addSong (string song, string artist){
+	
+	//untuk ngecek lagunya udah masuk playlist belum
+    for(auto i = judulLagu.begin(); i != judulLagu.end(); i++){
 
         if (*i == song){
-
+        	//kalo udah bakal keluar notif udah
             notifSongHasAddedToPlaylist = true;
             break;
 
         }
 
     }
-
+    
+//untuk nambahin lagu ke playlist
     if (notifSongHasAddedToPlaylist == false){
 
         judulLagu.push_back(song);
         penyanyi.push_back(artist);
 
-        playlistSize += 1;
+        playlistSize += 1; //pertambahan lagu dari 0
 
     }
     
 
 }
 
-vector<string> playlistSong(){
-
+//untuk ngirim data lagu
+vector <string> playlistSong(){
  return judulLagu;
-
 }
 
-vector<string> playlistArtist(){
-
+vector <string> playlistArtist(){
  return penyanyi;
-
 }
-
-
-//gambaran di fungsi Play
-// int main(){
-
-//    addSong("marigorudo", "aimyon");
-
-//    for (auto i = judulLagu.begin(); i != judulLagu.end(); i++){
-
-//         cout << *i << "\n";
-
-//    }
-
-// }
-
